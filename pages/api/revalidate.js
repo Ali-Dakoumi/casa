09b9f01b35bbfea3f4595ff8d0834de1cb24e9ea
Ali Dakoumi <dakoumi31@gmail.com>
2022-11-13 13:15:39 +0100

    send token

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,13 +1,11 @@
 export default async function handler(req, res) {
-  // if (req.query.secret !== process.env.NEXT_REVALIDATE_TOKEN) {
-  //   return res
-  //     .status(401)
-  //     .json({
-  //       message: "Invalid token",
-  //       token: req.query.secret,
-  //       processVar: process.env.NEXT_REVALIDATE_TOKEN,
-  //     });
-  // }
+  if (req.body.secret !== process.env.PRISMIC_WEBHOOK_SECRET) {
+    return res.status(401).json({
+      message: "Invalid token",
+      token: req.query.secret,
+      processVar: process.env.NEXT_REVALIDATE_TOKEN,
+    });
+  }
 
   try {
     await res.revalidate(req.query.path);
