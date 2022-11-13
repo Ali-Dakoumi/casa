8b23fc06f62294a6b3cb2efea11a14cbf734d3da
Ali Dakoumi <dakoumi31@gmail.com>
2022-11-13 17:50:19 +0100

    delete revalidate

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,8 +1,5 @@
 export default async function handler(req, res) {
-  if (
-    req.body.secret !== process.env.PRISMIC_WEBHOOK_SECRET &&
-    !req.body.secret
-  ) {
+  if (req.body.secret !== process.env.PRISMIC_WEBHOOK_SECRET) {
     return res.status(401).json({
       message: "Invalid token",
       token: req.query.secret,
@@ -14,8 +11,6 @@ export default async function handler(req, res) {
     await res.revalidate(req.query.path);
     return res.json({
       revalidated: true,
-      token: req.query.secret,
-      processVar: process.env.NEXT_REVALIDATE_TOKEN,
     });
   } catch (err) {
     return res.status(500).send("Error revalidating");
