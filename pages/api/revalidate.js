db4f9a100f69cfc2a09f61abe9c8057d5210ff47
Ali Dakoumi <dakoumi31@gmail.com>
2022-11-13 17:54:56 +0100

    delete revalidate

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -13,6 +13,6 @@ export default async function handler(req, res) {
       revalidated: true,
     });
   } catch (err) {
-    return res.status(500).send("Error revalidating");
+    return res.status(500).send("Error revalidating !!!");
   }
 }
