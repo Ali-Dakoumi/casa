737d128c2532aa924911649e21d02a4ceecddda6
Ali Dakoumi <dakoumi31@gmail.com>
2022-11-13 12:37:27 +0100

    send token

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -2,7 +2,11 @@ export default async function handler(req, res) {
   if (req.query.secret !== process.env.NEXT_REVALIDATE_TOKEN) {
     return res
       .status(401)
-      .json({ message: "Invalid token", token: req.query.secret });
+      .json({
+        message: "Invalid token",
+        token: req.query.secret,
+        processVar: process.env.NEXT_REVALIDATE_TOKEN,
+      });
   }
 
   try {
