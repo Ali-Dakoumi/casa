5704a9a51be2ece168ab589f49188cea95318cc9
Ali Dakoumi <dakoumi31@gmail.com>
2022-11-13 20:13:12 +0100

    delete revalidate

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,3 +1,6 @@
+import getApolloClient from "../../lib/client";
+import { ALL_POSTS } from "../../lib/query";
+
 export default async function handler(req, res) {
   // const { data, setData } = useContext(DataContext);
   // if (req.query.secret !== process.env.PRISMIC_WEBHOOK_SECRET) {
@@ -6,8 +9,13 @@ export default async function handler(req, res) {
 
   try {
     // await res.revalidate(req.query.path);
+    const client = getApolloClient();
+    const { data } = await client.query({
+      query: ALL_POSTS,
+    });
     return res.json({
       revalidated: true,
+      data: JSON.stringify(data),
     });
   } catch (err) {
     return res.status(500).send("Error revalidating !!! ");
